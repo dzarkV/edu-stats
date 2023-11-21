36501c7871a34e276fe6f220fe729e180bfe1828
dzarkV <dfvargasp@unal.edu.co>
2023-11-20 22:41:37 -0500

    formmating vue project

--- fe-edu-stats/src/main.js
+++ fe-edu-stats/src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-const app = createApp(App).mount('#app')
+const app = createApp(App).mount("#app");
